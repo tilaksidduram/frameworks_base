763980a95dfa734cb1cad795fa4eee2448a20dc8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-08-25 23:35:16 -0400

    Framework changes for moving applications to external volume
    * Add static functions for retrieving external apps volume information
    * Determine external apps volume using the 'externalApps' attribute in
      storage_list.xml
    * Allow moving applications to an external volume regardless if the
      internal storage is emulated or not
    * Check if app moving is disabled
    
    Thanks to Pierre-Hugues Husson for identifying an issue causing the
    SD card state to be incorrect (ie. SD card appearing unmounted when a
    USB drive is unplugged)
    
    Change-Id: I5561c2cf3f558b729c89663852173a532994a47a
    
    Conflicts:
    	services/java/com/android/server/MountService.java

--- services/java/com/android/server/MountService.java
+++ services/java/com/android/server/MountService.java
@@ -697,7 +697,7 @@ class MountService extends IMountService.Stub
 
         // Tell PackageManager about changes to primary volume state, but only
         // when not emulated.
-        if (volume.isPrimary() && !volume.isEmulated()) {
+        if (volume.isExternalApps()) {
             if (Environment.MEDIA_UNMOUNTED.equals(state)) {
                 mPms.updateExternalMediaStatus(false, false);
 
@@ -1072,7 +1072,9 @@ class MountService extends IMountService.Stub
         Runtime.getRuntime().gc();
 
         // Redundant probably. But no harm in updating state again.
-        mPms.updateExternalMediaStatus(false, false);
+        if (isExternalApps(path)) {
+            mPms.updateExternalMediaStatus(false, false);
+        }
         try {
             final Command cmd = new Command("volume", "unmount", path);
             if (removeEncryption) {
@@ -1259,18 +1261,21 @@ class MountService extends IMountService.Stub
                     // resource parser does not support longs, so XML value is in megabytes
                     long maxFileSize = a.getInt(
                             com.android.internal.R.styleable.Storage_maxFileSize, 0) * 1024L * 1024L;
+                    boolean externalApps = a.getBoolean(
+                            com.android.internal.R.styleable.Storage_externalApps, false);
 
                     Slog.d(TAG, "got storage path: " + path + " description: " + description +
                             " primary: " + primary + " removable: " + removable +
                             " emulated: " + emulated +  " mtpReserve: " + mtpReserve +
                             " allowMassStorage: " + allowMassStorage +
-                            " maxFileSize: " + maxFileSize);
+                            " maxFileSize: " + maxFileSize +
+                            " externalApps: " + externalApps);
 
                     if (emulated) {
                         // For devices with emulated storage, we create separate
                         // volumes for each known user.
                         mEmulatedTemplate = new StorageVolume(null, descriptionId, true, false,
-                                true, mtpReserve, false, maxFileSize, null);
+                                true, mtpReserve, false, maxFileSize, null, externalApps);
 
                         final UserManagerService userManager = UserManagerService.getInstance();
                         for (UserInfo user : userManager.getUsers(false)) {
@@ -1283,7 +1288,7 @@ class MountService extends IMountService.Stub
                         } else {
                             final StorageVolume volume = new StorageVolume(new File(path),
                                     descriptionId, primary, removable, emulated, mtpReserve,
-                                    allowMassStorage, maxFileSize, null);
+                                    allowMassStorage, maxFileSize, null, externalApps);
                             addVolumeLocked(volume);
 
                             // Until we hear otherwise, treat as unmounted
@@ -1533,6 +1538,17 @@ class MountService extends IMountService.Stub
         }
         synchronized (mListeners) {
             return mUmsAvailable;
+        }
+    }
+
+    private boolean isExternalApps(String path) {
+        synchronized (mVolumesLock) {
+            for (StorageVolume volume : mVolumes) {
+                if (volume.isExternalApps() && volume.getPath().equals(path)) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
