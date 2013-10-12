12982818eb77d63c52e0c4a4d65b8e9851456abb
Jim Miller <jaggies@google.com>
2013-10-11 21:03:22 -0700

    Fix problem where PhoneWindowManager waits for keyguard to draw
    This fixes a problem where PhoneWindowManager times out waiting
    for keyguard to draw when it's disabled.  Instead, we pass a null
    token back which allows PhoneWindowManager to continue.
    
    Fixes bug 11190932
    
    Change-Id: I041ea1d88dca681476d9477a3aab54559976b312
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -650,11 +650,11 @@ public class KeyguardViewManager {
 
         final IBinder token;
 
-        // If keyguard is disabled or not showing, we need to inform PhoneWindowManager with a null
+        // If keyguard is disabled, we need to inform PhoneWindowManager with a null
         // token so it doesn't wait for us to draw...
         final boolean disabled =
                 mLockPatternUtils.isLockScreenDisabled() && !mLockPatternUtils.isSecure();
-        if (!isShowing() || disabled) {
+        if (mKeyguardHost == null || disabled) {
             token = null;
         } else {
             token = mKeyguardHost.getWindowToken();
