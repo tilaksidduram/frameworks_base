f6b676d5c6031087be5f9314e9dae4f3611763ca
helicopter88 <johnlemon.lol@gmail.com>
2013-11-28 22:24:12 +0100

    Frameworks: Policy: implement a pin keypad shuffler [1/2]
    this is to further prevent people using the tablet guessing your pin based on fingerprints left on the screen
    example of the expected outcome - https://www.dropbox.com/sh/r3xb2q53ksaqni9/Oz1Eu8Dm7O
    
    - correct coding style
    - added reload button
    
    PatchSet:
    - simplify shuffle code
    - fix over shifting in some cases
    - add reload button functionallity
    - add lockscreenutils method to detect only PIN lock
      for settings
    - exclude sim and simpuk PIN lock
    - add drawables for reload button
    
    PatchSet
    - cleanup
    
    PatchSet:
    - fix shuffle NumKeyPad even if a theme replaces the xml
      and reload button is not present.
    
    PatchSet:
    - add mode disabled/always/on request
    
    cheers
    
    kufi
    
    Ported to new keyguard package on 4.4
    
    Change-Id: I6cf23a70a24ff0d640850d35acd4584e2e8d2192
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1030,6 +1030,7 @@ public final class Settings {
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_ENABLED);
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_VISIBLE);
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED);
+            MOVED_TO_SECURE.add(Secure.LOCK_NUMPAD_RANDOM);
             MOVED_TO_SECURE.add(Secure.LOCK_BEFORE_UNLOCK);
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_SIZE);
             MOVED_TO_SECURE.add(Secure.LOGGING_ID);
@@ -4940,6 +4941,14 @@ public final class Settings {
          * @hide
          */
         public static final String LOCKSCREEN_TARGETS_COLOR = "lockscreen_targets_color";
+
+        /**
+         * Whether the NumKeyPad will change the orders of numbers
+         * in a PIN locked lockscreen
+         * 0 = off | 1 = always | 2 = only on request
+         * @hide
+          */
+        public static final String LOCK_NUMPAD_RANDOM = "lock_numpad_random";
 
          /**
          *Whether lock before unlock is enabled or disabled
