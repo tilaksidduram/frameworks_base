33d7da3ef834576da902b8a11bbc0e7959aab615
Lars Greiss <kufikugel@googlemail.com>
2014-01-28 12:35:49 +0100

    fb: unbreak recents animations on thumbnail click or dismiss recents screen
    We load now correctly the recents screen and preload the first task on press
    down off any key to get always recents without issues up. We cancel only
    if any other key was pressed. I discovered during I am working on new recents
    that this solves all issues but has one downside that if he user dismiss he recents
    screen or click on one of the thumbnails to bring the choosen app to forground
    that of course the last thumbnail is not cleared. Due that the entry animation
    and some other things depent on what is currently preloaded we need to cancel
    first task to have always in all cases the right animation.
    
    Fix it with just calling cancelPreloadingFirstTask() if the user either
    clicked on one of the thumbnails or dismissed the recents screen.
    
    PS
    - do the same when all thubmnails are swiped away
    PS
    - derp
    
    Change-Id: I786c05d929f6a02b6d4928fc4a96aeb9a8e2d03e

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -745,6 +745,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         if (usingDrawingCache) {
             holder.thumbnailViewImage.setDrawingCacheEnabled(false);
         }
+        RecentTasksLoader.getInstance(mContext).cancelPreloadingFirstTask();
     }
 
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -766,6 +767,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         // mListAdapter.notifyDataSetChanged();
 
         if (mRecentTaskDescriptions.size() == 0) {
+            RecentTasksLoader.getInstance(mContext).cancelPreloadingFirstTask();
             dismissAndGoBack();
         }
 
