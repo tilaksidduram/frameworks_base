cb5f1a757d27c883be365809c5b3d54653de8281
tilaksidduram <tilaksidduram@gmail.com>
2014-03-25 21:59:31 +0530

    Revert "Quicksettings: Volume tile"
    This reverts commit 88a0535ccf822068ab214f2b312eef7eb7468b27.
    
    Conflicts:
    	packages/SystemUI/res/values/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -41,7 +41,6 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.hardware.display.DisplayManager;
-import android.media.AudioManager;
 import android.media.MediaRouter;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
@@ -115,8 +114,7 @@ class QuickSettings {
         LIGHTBULB,
         NFC,
         SLEEP,
-        QUITEHOUR,
-	VOLUME
+        QUITEHOUR
     }
 
     public static final String NO_TILES = "NO_TILES";
@@ -1017,31 +1015,6 @@ class QuickSettings {
                     });
                     parent.addView(sleepTile);
                     if(addMissing) sleepTile.setVisibility(View.GONE);
-		// Volume tile
-		} else if(Tile.VOLUME.toString().equals(tile.toString())) {                     
-                    final QuickSettingsBasicTile volumeTile
-                    = new QuickSettingsBasicTile(mContext);
-                    volumeTile.setTileId(Tile.VOLUME);
-                    volumeTile.setImageResource(R.drawable.ic_qs_volume);
-                    volumeTile.setTextResource(R.string.quick_settings_volume_label);
-                    volumeTile.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            collapsePanels();
-                      AudioManager am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
-                      am.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
-                        }
-                    });
-
-                    volumeTile.setOnLongClickListener(new View.OnLongClickListener() {
-                        @Override
-                        public boolean onLongClick(View v) {
-                            startSettingsActivity(android.provider.Settings.ACTION_SOUND_SETTINGS);
-                      return true;
-                        }
-                    });
-                    parent.addView(volumeTile);
-                    if(addMissing) volumeTile.setVisibility(View.GONE);
                 } else if(Tile.NFC.toString().equals(tile.toString()) && isNfcSupported()) {
                     final QuickSettingsBasicTile nfcTile = new QuickSettingsBasicTile(mContext);
                     nfcTile.setTileId(Tile.NFC);
