9ae70612b025914678b69360834d150b6adce431
tilaksidduram <tilaksidduram@gmail.com>
2014-05-14 00:19:42 +0530

    Revert "Lockscreen Blur: fix for coverflows"
    This reverts commit 879776ca235e5195c75d7db57caef193dd4024e1.

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -104,6 +104,7 @@ public class KeyguardViewManager {
     private LockPatternUtils mLockPatternUtils;
 
     private Bitmap mBlurredImage = null;
+    private int mLastRotation = 0;
     private int mBlurRadius = 12;
     private boolean mSeeThrough = false;
     private boolean mIsCoverflow = true;
@@ -115,11 +116,17 @@ public class KeyguardViewManager {
     private KeyguardUpdateMonitorCallback mBackgroundChanger = new KeyguardUpdateMonitorCallback() {
         @Override
         public void onSetBackground(Bitmap bmp) {
-            if (bmp != null) mBlurredImage = null;
-            mIsCoverflow = true;
-            setCustomBackground (bmp);
+            mKeyguardHost.setCustomBackground( new BitmapDrawable(mContext.getResources(),
+                        bmp != null ? bmp : mBlurredImage) );
+            updateShowWallpaper(bmp == null && mBlurredImage == null);
         }
     };
+
+    private void setCustomBackground(Bitmap bmp) {
+        mKeyguardHost.setCustomBackground(bmp != null ?
+                    new BitmapDrawable(mContext.getResources(), bmp) : null);
+        updateShowWallpaper(bmp == null);
+    }
 
     public interface ShowListener {
         void onShown(IBinder windowToken);
@@ -227,12 +234,6 @@ public class KeyguardViewManager {
     private boolean shouldEnableTranslucentDecor() {
         Resources res = mContext.getResources();
         return res.getBoolean(R.bool.config_enableLockScreenTranslucentDecor);
-    }
-
-    private void setCustomBackground(Bitmap bmp) {
-        mKeyguardHost.setCustomBackground( new BitmapDrawable(mContext.getResources(),
-                    bmp != null ? bmp : mBlurredImage) );
-        updateShowWallpaper(bmp == null && mBlurredImage == null);
     }
 
     public void setBackgroundBitmap(Bitmap bmp) {
@@ -501,7 +502,7 @@ public class KeyguardViewManager {
             mBlurredImage = rotateBmp(mBlurredImage, mLastRotation - currentRotation);
             mLastRotation = currentRotation;
             mIsCoverflow = false;
-            setCustomBackground(mBlurredImage);
+            KeyguardUpdateMonitor.getInstance(mContext).dispatchSetBackground(mBlurredImage);
         } else {
             mIsCoverflow = true;
         }
