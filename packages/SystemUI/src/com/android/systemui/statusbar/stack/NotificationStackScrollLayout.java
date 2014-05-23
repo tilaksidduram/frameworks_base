990618353d9307f993a88849a8aaf5cd9bb63294
oreiche <oliver.reiche@gmail.com>
2014-05-23 17:31:24 +0200

    SystemUI: Extend swipe helper (Part1)
    I was not happy with long press on notifications and recents
    to open apps in floating mode. The long press duration kinda
    breaks the UI interaction flow, which prevented me from
    actually using floating windows. Therefore, I've created a
    private modification, which enables floating mode for
    notifications and recents by swiping to the left. This
    greatly improves usability without strongly deviating from
    stock UI experience, in strict accordance with PA's
    philosophy.  Changes are surgically implemented to keep the
    probability of merge conflicts rather low. The code was
    successfully tested on mako, hammerhead, tilapia, and manta.
    Let the team discuss and decide whether or not this is a
    worthy extension to PA.
    
    Changes are split into two commits.
    
    - Extending SwipeHelper:
    SwipeHelper can now distinguish between directions LEFT,
    RIGHT, UP, DOWN and can fire an abstract trigger 'event'. A
    trigger direction can be specified and when a swipe in this
    direction occurs dismissable children will not be dismissed,
    instead they will be snapped back.  Whenever a child is
    swiped far or fast enough in that direction an event is
    triggered and the onChildTriggerd() callback is issued. This
    callback was added to the SwipeHelper.Callback interface and
    therefore requires all its implementations to realize this
    method, such as probably Hover, which is sadly not open
    source (yet?).
    
    Change-Id: Ida06195913f022463d29ca22092e81fb8993ad50

--- packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -628,6 +628,9 @@ public class NotificationStackScrollLayout extends ViewGroup
         return false;
     }
 
+    public void onChildTriggered(View v) {
+    }
+
     public void onBeginDrag(View v) {
         setSwipingInProgress(true);
         mAmbientState.onBeginDrag(v);
