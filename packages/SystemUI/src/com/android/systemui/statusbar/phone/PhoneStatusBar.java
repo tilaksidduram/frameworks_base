cb7eab9a4fd2ba626ee9e969782c48b1c467de81
medsouz <medsouz99@gmail.com>
2014-05-26 18:39:30 -0400

    Show IME back button in Pie.
    Updated Pie to show the alternate back button when an IME is present like the stock navigation bar does.
    
    Change-Id: I97b385904c183a42c6d76a9f238c4b9624668576
    Picture: http://i.imgur.com/T0n9bVc.png
    
    EDIT: Fixed whitespace and tabbing
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -116,6 +116,7 @@ import com.android.systemui.BatteryCircleMeterView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogTags;
 import com.android.systemui.R;
+import com.android.systemui.recent.NavigationCallback;
 import com.android.systemui.recent.RecentsActivity;
 import com.android.systemui.ReminderMessageView;
 import com.android.systemui.statusbar.BaseStatusBar;
@@ -2738,6 +2739,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         if (mNavigationBarView != null) {
             mNavigationBarView.setNavigationIconHints(hints);
+        }
+        if(mPieController != null) {
+            mPieController.setNavigationIconHints(NavigationCallback.NAVBAR_BACK_HINT, hints, false);
         }
         checkBarModes();
     }
