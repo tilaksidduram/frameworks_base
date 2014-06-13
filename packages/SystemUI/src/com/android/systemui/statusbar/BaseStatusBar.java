678e6da897130766023d7c59a4889411d97bf2ce
BigBrother1984 <stevewatersy@gmail.com>
2014-06-13 14:30:20 +0200

    PIE: Improve assistant handling
    Run it from SearchPanelView and determinate before, at PieMenu creation,
    if we have any assistant available or not.
    
    Some cleanup too.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I986a5e5257f432acd58dd2d75fc13f2b772c7453

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -261,6 +261,10 @@ public abstract class BaseStatusBar extends SystemUI implements
         return mNotificationData;
     }
 
+    public SearchPanelView getSearchPanelView() {
+        return mSearchPanelView;
+    }
+
     public RemoteViews.OnClickHandler getNotificationClickHandler() {
         return mOnClickHandler;
     }
