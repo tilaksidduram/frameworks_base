27da98017dd0b2e4afc31125e88b93eda832b7ac
Jorge Ruesga <jorge@ruesga.com>
2014-07-11 01:31:25 +0200

    base: update link ring-notifications after read user settings
    Change-Id: Ib424124dc20e38243e572333e323ba97aacb1111
    JIRA: CYAN-3559
    Issue: https://jira.cyanogenmod.org/browse/CYAN-3559
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -546,6 +546,8 @@ public class AudioService extends IAudioService.Stub {
         // array initialized by updateStreamVolumeAlias()
         updateStreamVolumeAlias(false /*updateVolumes*/);
         readPersistedSettings();
+        // must update link ring-notifications streams once the user preferences were read
+        updateLinkNotificationStreamVolumeAlias();
         mSettingsObserver = new SettingsObserver();
         //Update volumes steps before creatingStreamStates!
         initVolumeSteps();
@@ -741,11 +743,7 @@ public class AudioService extends IAudioService.Stub {
         }
         mStreamVolumeAlias[AudioSystem.STREAM_DTMF] = dtmfStreamAlias;
 
-        if (mLinkNotificationWithVolume) {
-            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_RING;
-        } else {
-            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_NOTIFICATION;
-        }
+        updateLinkNotificationStreamVolumeAlias();
 
         if (updateVolumes) {
             mStreamStates[AudioSystem.STREAM_DTMF].setAllIndexes(mStreamStates[dtmfStreamAlias]);
@@ -757,6 +755,14 @@ public class AudioService extends IAudioService.Stub {
                     0,
                     0,
                     mStreamStates[AudioSystem.STREAM_DTMF], 0);
+        }
+    }
+
+    private void updateLinkNotificationStreamVolumeAlias() {
+        if (mLinkNotificationWithVolume) {
+            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_RING;
+        } else {
+            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_NOTIFICATION;
         }
     }
 
