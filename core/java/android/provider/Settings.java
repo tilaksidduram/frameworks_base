321da1fcb6dfd99a36b890b3465fbb9d6c9fdc64
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-12-01 22:08:24 +0200

    SystemUI: Add option to disable search bar in recents (2/2)
    Coded from scratch by yashade2001.
    Firstly written for Project D.I.S.C.O. ROM.
    Thanks to TheMrcool212 for his help.
    
    Change-Id: I3f985aab873d05350c8301bdedae5eeee49f70c7
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3359,6 +3359,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
+         * Show search bar in recents
+         * @hide
+         */
+        public static final String RECENTS_SHOW_SEARCH_BAR = "recents_show_search_bar";
+
+        /**
          * Whether to enable voice wakeup.  The value is boolean (1 or 0).
          * @hide
          */
