e5c0925e14e53d334782aab4d608970b2907cd31
cristianomatos <cristianobmatos@gmail.com>
2014-12-31 10:33:44 -0200

    AOKP system animations: remove no app override
    - Removed until we find a better solution regarding recents animations

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4047,8 +4047,7 @@ public final class Settings {
                 "task_open_behind",
         };  
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
-
+        
         /**
          * Enable built in CAF task manager
          * @hide
