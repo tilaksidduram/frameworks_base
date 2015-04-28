0bb59636b59743d156d0c3067bd70e39ea937a32
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-04-28 20:42:00 +0100

    Base: Add a controller for in-call proximity sensor (1/3)
    Change-Id: Icd17599ef3e0c45ee10fede7d663ea9a30446257
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3418,6 +3418,12 @@ public final class Settings {
         public static final String BRIGHTNESS_ICON = "brightness_icon";
 
         /**
+         * Whether to use the proximity sensor to turn the screen on/off during a call
+         * @hide
+         */
+        public static final String IN_CALL_PROXIMITY_SENSOR = "in_call_proximity_sensor";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
