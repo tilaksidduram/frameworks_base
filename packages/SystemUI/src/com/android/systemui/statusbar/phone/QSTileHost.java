261803d080d22b8744491b39f3e6f26cf28042db
rogersb11 <brettrogesr11@gmail.com>
2015-05-08 02:06:29 +0200

    QS: Pie Control tile
    Change-Id: I1904913adfc08d197a9ba4a802b9ebfd2f4f09ac
    Signed-off-by: rogersb11 <brettrogesr11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -47,6 +47,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
+import com.android.systemui.qs.tiles.PieControlTile;
 import com.android.systemui.qs.tiles.RoamingTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -279,6 +280,7 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals(ImmersiveTile.SPEC)) return new ImmersiveTile(this);
         else if (tileSpec.equals(LiveDisplayTile.SPEC)) return new LiveDisplayTile(this);
         else if (tileSpec.equals(NotificationsTile.SPEC)) return new NotificationsTile(this);
+        else if (tileSpec.equals(PieControlTile.SPEC)) return new PieControlTile(this);
         else if (tileSpec.equals(DataTile.SPEC)
                 && mConnectivityManager.isNetworkSupported(ConnectivityManager.TYPE_MOBILE))
             return new DataTile(this);
