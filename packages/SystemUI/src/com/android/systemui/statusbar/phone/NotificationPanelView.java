c4230433eb66801a7221c5a26d5290fa1d8879c9
Susan McCombs <smac0628@gmail.com>
2015-05-25 14:59:01 +0200

    Fix ForceClose due to TaskManger.
    Change-Id: I665a366abe857c51dbb111a57048153527ab1007
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -363,6 +363,7 @@ public class NotificationPanelView extends PanelView implements
 	if (mQsSizeChangeAnimator == null) {
 	    mQsContainer.setHeightOverride(mQsContainer.getDesiredHeight());
         }
+        requestPanelHeightUpdate();
     }
 
     @Override
