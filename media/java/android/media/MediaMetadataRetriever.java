81ffeb3f61e548d0eeb8902de09c43157a94833a
tilaksidduram <tilaksidduram@gmail.com>
2015-08-12 22:58:47 +0530

    Revert "Fix filedescriptor leakage in MediaMetadataRetriever"
    This reverts commit 04909139f3c8bf339d703ad2b956f8457595c1ce.

--- media/java/android/media/MediaMetadataRetriever.java
+++ media/java/android/media/MediaMetadataRetriever.java
@@ -71,12 +71,6 @@ public class MediaMetadataRetriever
             throw new IllegalArgumentException();
         } catch (IOException ioEx) {
             throw new IllegalArgumentException();
-        } finally {
-            try {
-                if (is != null) {
-                    is.close();
-                }
-            } catch (Exception e) {}
         }
     }
 
