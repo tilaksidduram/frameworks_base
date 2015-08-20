30498d7eecd5caa7216f6a8dfb5573a1b32fc04c
d34d <clark@cyngn.com>
2015-08-20 12:29:14 -0700

    Themes: Allow packages to be set as non-themeable
    This patch allows for specific packages to be declared as non
    themeable.  Packages that are non themeable will not have theme
    resources attached to their assets when their Resources object
    is created.
    
    This patch also removes isThemeable from CompatibilityInfo as this
    object did not correctly reflect that the package is not themeable.
    
    Change-Id: Id34c63ec0c3ef7c69df083da63559e0720ce0018
    REF: CYNGNOS-770

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -1668,7 +1668,7 @@ public final class ActivityThread {
             LoadedApk pkgInfo, Context context, String pkgName) {
         return mResourcesManager.getTopLevelResources(resDir, splitResDirs, overlayDirs, libDirs,
                 displayId, pkgName, overrideConfiguration, pkgInfo.getCompatibilityInfo(), null,
-                context);
+                context, pkgInfo.getApplicationInfo().isThemeable);
     }
 
     /**
@@ -1677,7 +1677,8 @@ public final class ActivityThread {
     Resources getTopLevelThemedResources(String resDir, int displayId, LoadedApk pkgInfo,
                                          String pkgName, String themePkgName) {
         return mResourcesManager.getTopLevelThemedResources(resDir, displayId, pkgName,
-                themePkgName, pkgInfo.getCompatibilityInfo(), null);
+                themePkgName, pkgInfo.getCompatibilityInfo(), null,
+                pkgInfo.getApplicationInfo().isThemeable);
     }
 
     final Handler getHandler() {
