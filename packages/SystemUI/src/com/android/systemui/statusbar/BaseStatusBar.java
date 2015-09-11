94e96b858617608d8b18eb0b805d4dc8157c8344
Adnan Begovic <adnan@cyngn.com>
2015-09-10 20:56:27 -0700

    SystemUi: Prioritize call notifications which interrupt.
      Posting notifications to the front of the message queue in the
      SystemUi handler can save 800ms -> 1.5s of real time when receiving
      and call notifications under heavy load.
    
    TICKET: CYNGNOS-969
    Change-Id: I56e4a9482e8f8cc8fed97cdb4a46625ddddcf277
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -515,7 +515,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         public void onNotificationPosted(final StatusBarNotification sbn,
                 final RankingMap rankingMap) {
             if (DEBUG) Log.d(TAG, "onNotificationPosted: " + sbn);
-            mHandler.post(new Runnable() {
+            final Runnable notificationPost = new Runnable() {
                 @Override
                 public void run() {
                     Notification n = sbn.getNotification();
@@ -545,7 +545,12 @@ public abstract class BaseStatusBar extends SystemUI implements
                         addNotification(sbn, rankingMap);
                     }
                 }
-            });
+            };
+            if (TextUtils.equals(sbn.getNotification().category, Notification.CATEGORY_CALL)) {
+                mHandler.postAtFrontOfQueue(notificationPost);
+            } else {
+                mHandler.post(notificationPost);
+            }
         }
 
         @Override
@@ -1362,7 +1367,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     public abstract void resetHeadsUpDecayTimer();
     public abstract void hideHeadsUp();
 
-    public abstract void scheduleHeadsUpOpen();
+    public abstract void scheduleHeadsUpOpen(boolean immediate);
 
     public abstract void scheduleHeadsUpClose();
 
--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -161,7 +161,7 @@ public class TvStatusBar extends BaseStatusBar {
     }
 
     @Override
-    public void scheduleHeadsUpOpen() {
+    public void scheduleHeadsUpOpen(boolean immediate) {
     }
 
     @Override
