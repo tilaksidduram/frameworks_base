f6bce36aab2c36ce0b8d3ef894b6600fbb7d7a18
Sultanxda <sultanxda@gmail.com>
2015-09-12 22:25:03 -0700

    telephony: Handle NPE in setDataEnabledUsingSubId()
    This should fix the following crash:
    
    E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: main
    E AndroidRuntime: java.lang.RuntimeException: Error receiving broadcast Intent { act=android.net.wifi.STATE_CHANGE flg=0x44000010 (has extras) } in com.android.server.ProfileTriggerHelper@229e56b3
    ...
    E AndroidRuntime: Caused by: java.lang.NullPointerException: Attempt to invoke interface method 'void com.android.internal.telephony.ITelephony.setDataEnabled(int, boolean)' on a null object reference
    E AndroidRuntime: 	at android.telephony.TelephonyManager.setDataEnabledUsingSubId(TelephonyManager.java:3845)
    
    Change-Id: I046ff7e5e2b8de1717a461c08813100b10fcc6e5
    
    Patchset: 1
    http://review.cyanogenmod.org/#/c/109411/

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -3812,6 +3812,8 @@ public class TelephonyManager {
             getITelephony().setDataEnabled(subId, enable);
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling setDataEnabled", e);
+        } catch (NullPointerException npe) {
+            Log.e(TAG, "Error calling setDataEnabled", npe);
         }
     }
 
