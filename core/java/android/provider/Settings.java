706fe266f6a31bd0f8a4aea28fcea35a90bb4f41
tilaksidduram <tilaksidduram@gmail.com>
2015-10-27 19:58:45 +0530

    Revert "Framework: StatusBar Ticker (1/2)"
    This reverts commit 4d47ef446e3815c8931c1d31339e3e205dd2dfdb.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3614,13 +3614,6 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
 
         /**
-         * Whether to enable ticker animation
-         *
-         * @hide
-         */
-        public static final String TICKER_ENABLED = "ticker_enabled";
-
-        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
