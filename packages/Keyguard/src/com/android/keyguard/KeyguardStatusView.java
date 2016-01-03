127f7d0c4227e130aea5ba4195d779ca8de87ebc
Janson Kang <temasek71@gmail.com>
2016-01-03 19:33:26 +0800

    Revert "Fix NPE with WeatherStatus on KeyguardStatusView"
    This reverts commit 401c877d0d5055b7cd84aec3b011b5b322f93dd5.

--- packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
@@ -339,10 +339,8 @@ public class KeyguardStatusView extends GridLayout implements
             }
         }
 
-        if (mWeatherView != null) {
-            mWeatherView.setVisibility(
+        mWeatherView.setVisibility(
                 (mShowWeather && !forceHideByNumberOfNotifications) ? View.VISIBLE : View.GONE);
-        }
         if (forceHide) {
             mWeatherView.setVisibility(View.GONE);
         } else {
