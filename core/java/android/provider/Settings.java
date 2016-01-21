75e20897110a5973fcf010656ad89e342556f087
LorDClockaN <davor@losinj.com>
2016-01-21 22:05:36 +0100

    FWB: Power/reboot menu transparency (1/2)
    Change-Id: Ib703c2574aca9bc3c534a155d165f07b603c0535

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4776,6 +4776,12 @@ public final class Settings {
         public static final String DASHBOARD_COLUMNS = "dashboard_columns";
 
         /**
+         * Transparent power menu and dialogs
+         * @hide
+         */
+        public static final String TRANSPARENT_POWER_MENU = "transparent_power_menu";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
