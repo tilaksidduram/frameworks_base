8bbf9551aaec02d2849c782e3ebf33fc6bca604c
LorDClockaN <davor@losinj.com>
2016-02-08 15:08:02 +0100

    FWB: App picker tile (1/2)
    Reused navbar icon and string
    
    Change-Id: Ic7a8729749ecdcbbe4e7b5744d64b758645bb34a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.QSTile;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
+import com.android.systemui.qs.tiles.AppPickerTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.BrightnessTile;
@@ -407,6 +408,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("battery_saver")) return new BatterySaverTile(this);
         else if (tileSpec.equals("power_menu")) return new PowerMenuTile(this);
 	else if (tileSpec.equals("themes")) return new ThemesTile(this);
+        else if (tileSpec.equals("app_picker")) return new AppPickerTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -514,6 +516,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("battery_saver")) return R.string.quick_settings_battery_saver_label;
         else if (spec.equals("power_menu")) return R.string.quick_settings_power_menu_label;
 	else if (spec.equals("themes")) return R.string.quick_settings_themes;
+        else if (spec.equals("app_picker")) return R.string.navbar_app_picker;
         return 0;
     }
 
@@ -558,6 +561,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("battery_saver")) return R.drawable.ic_qs_battery_saver_on;
         else if (spec.equals("power_menu")) return R.drawable.ic_qs_power_menu;
 	else if (spec.equals("themes")) return R.drawable.ic_qs_themes;
+        else if (spec.equals("app_picker")) return R.drawable.ic_sysbar_app_picker;
         return 0;
     }
 
