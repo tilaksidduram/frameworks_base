93beb2a17c9432e9257bcebff4c0fd4ce2b2d0d2
Alex Cruz <mazdarider23@gmail.com>
2016-02-26 04:44:13 -0500

    Toggle Dashboard switches [2/2]
    Change-Id: If7970bc06b9323423a152435a869dc3e80156966

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4983,6 +4983,13 @@ public final class Settings {
         public static final String DASHBOARD_COLUMNS = "dashboard_columns";
 
         /**
+         *
+         * Whether to show switches for wifi/cell/bt in the settings dashboard
+         * @hide
+         */
+        public static final String DASHBOARD_SWITCHES = "dashboard_switches";
+
+        /**
          * Transparent power menu and dialogs
          * @hide
          */
