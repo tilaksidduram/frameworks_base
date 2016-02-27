91f92ce4cea2774b83e903a0d91666776259c42c
Varun <date.varun123@gmail.com>
2016-02-27 17:56:29 +0530

    Recents: More Buttons and Colors[1/2]
    --Recents FullScreen Date Color
    --Recents FullScreen Clock Color
    
    --16 New Clear-All Icons
    
    Credits : thanks to LorDClockaN ( Davor Bertovic ) for Recents FullScreen Clock and Date Commit
    https://github.com/AICP/frameworks_base/commit/eb0d6e762d445836d4b80b6e8d7e6bd2d5c62b59
    
    Clock & Date Colors Based on Textviews Defined Above
    
    Signed-off-by: Varun <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3780,6 +3780,18 @@ public final class Settings {
          */
         public static final String CLEAR_BUTTON_COLOR= "clear_button_color";
 
+       /**
+         * Recents Date Text Color
+         * @hide
+         */
+        public static final String RECENTS_DATE_COLOR= "recents_date_color";
+
+       /**
+         * Recents Clock Text Color
+         * @hide
+         */
+        public static final String RECENTS_CLOCK_COLOR= "recents_clock_color";
+
         /**
          * Sets the date string style
          * 0 - Regular style
