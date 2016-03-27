759a8710c8d9a8a79004b85e4c10c194be199d14
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: Ic0d899879dcf6b324c270e7a33c3c62a664c7df8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4313,6 +4313,12 @@ public final class Settings {
         public static final String HIDE_LOCKSCREEN_ALARM = "hide_lockscreen_alarm";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Timeout for ambient display notification
          * @hide
          */
