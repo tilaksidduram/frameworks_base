1cfab135609d8d18cf666f82a96215ed6e15c0c6
yank555-lu <yank555.lu@gmail.com>
2016-04-04 01:24:27 -0700

    Frameworks_base: add dt2w on doze [2/2]
    Thanx to Patrick for this excellent idea and to Dankoman for pointing me in the right direction ;)
    
    Fix dt2s : ignore touch events for dt2s while dozing
    
    PS2: fix "swipe down turns display off and disables doze until screen is turned on at least once"
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5117,6 +5117,12 @@ public final class Settings {
         public static final String SLIM_ACTION_FLOATS = "slim_action_floats";
 
         /**
+         * Require double tap instead of simple tap to wake from Doze pulse screen
+         * @hide
+         */
+        public static final String DOZE_WAKEUP_DOUBLETAP = "doze_wakeup_doubletap";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
