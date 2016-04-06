43897f996620de29f8251b4ca791c058e6246126
Danesh M <daneshm90@gmail.com>
2016-04-06 14:18:05 -0700

    Lockscreen target fixes
    - Ensure icon's raw screen coordinates are used
    to calculate intersection.
    - Ensure we expand/collapse when running hint animation
      for unlock due to bounce.
    - Ensure we don't animate window bound changes.
    
    Change-Id: Id18e2237c1ce9bfb2bcaa669934a4b3af4341521
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -481,7 +481,6 @@ public class NotificationPanelView extends PanelView implements
                 // Ensure we collapse and clear fade
                 if (action == MotionEvent.ACTION_UP ||
                         action == MotionEvent.ACTION_CANCEL) {
-                    mKeyguardBottomArea.expand(false);
                     mKeyguardBottomArea.setBackground(null);
                 }
 
@@ -2356,6 +2355,7 @@ public class NotificationPanelView extends PanelView implements
 
     @Override
     protected void startUnlockHintAnimation() {
+        mKeyguardBottomArea.expand(true);
         super.startUnlockHintAnimation();
         startHighlightIconAnimation(getCenterIcon());
     }
