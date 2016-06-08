36af193fcd62bed8ad6edcaf23902eeed11403dc
LorDClockaN <lordclockan@gmail.com>
2016-06-08 22:30:49 +0200

    Fix a glitch causing shadow not to be reset when header disabled
    Observer is a GOD
    
    Change-Id: Ice75b19eee5a59963d54b3efa2528acc42fa78ce

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -197,6 +197,9 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private boolean mQsColorSwitch = false ;	
     private int mHeaderColor;
 
+    private int headerShadow;
+    private int customHeader;
+
     // Font style
     public static final int FONT_NORMAL = 0;
     public static final int FONT_ITALIC = 1;
@@ -1292,6 +1295,10 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                     Settings.System.QS_COLOR_SWITCH), false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ENABLE_TASK_MANAGER), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER), false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -1338,6 +1345,14 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             mShowTaskManager = Settings.System.getIntForUser(resolver,
                 Settings.System.ENABLE_TASK_MANAGER, 0, currentUserId) == 1;
 
+            headerShadow = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, 0,
+                    UserHandle.USER_CURRENT);
+
+            customHeader = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
+                    UserHandle.USER_CURRENT);
+
             updateVisibilities();
             requestCaptureValues();
         }
@@ -1350,6 +1365,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                 mBackgroundImage.setVisibility(View.VISIBLE);
                 setNotificationPanelHeaderBackground(next, force);
                 mCurrentBackground = next;
+                applyHeaderBackgroundShadow();
             }
         } else {
             mCurrentBackground = null;
@@ -1374,13 +1390,13 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     }
 
     private void applyHeaderBackgroundShadow() {
-        final int headerShadow = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, 0,
-                UserHandle.USER_CURRENT);
-
-        if (mBackgroundImage != null) {
+        if (customHeader == 1) {
             ColorDrawable shadow = new ColorDrawable(Color.BLACK);
             shadow.setAlpha(headerShadow);
+            mBackgroundImage.setForeground(shadow);
+        } else if (customHeader == 0) {
+            ColorDrawable shadow = new ColorDrawable(Color.BLACK);
+            shadow.setAlpha(0);
             mBackgroundImage.setForeground(shadow);
         }
     }
