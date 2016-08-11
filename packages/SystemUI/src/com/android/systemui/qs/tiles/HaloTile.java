5019209e97c9cc057b962ec6c4cb97c5345d8304
Varun Date <date.varun123@gmail.com>
2016-08-11 13:19:11 +0530

    Update Halo Tile Longpress Activity
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/HaloTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HaloTile.java
@@ -60,7 +60,6 @@ public class HaloTile extends QSTile<QSTile.BooleanState> {
         return CMMetricsLogger.DONT_LOG;
     }
 
-
     @Override
     protected void handleClick() {
         toggleState();
@@ -73,6 +72,10 @@ public class HaloTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     public void handleLongClick() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName("com.android.settings",
+            "com.android.settings.Settings$HaloSettingsActivity");
+        mHost.startActivityDismissingKeyguard(intent);
     }
 
     protected void toggleState() {
