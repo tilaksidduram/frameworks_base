3643dc9d986c7b0f6bcbc511910ae569db0e016c
tilaksidduram <tilaksidduram@gmail.com>
2016-08-12 16:22:19 +0530

    Revert "FWB: BootDialog: Device model instead of AICP for title + custom title (1/2)"
    This reverts commit d2b4bd8f3ad974d09966d9ad90322fb882bab855.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -63,7 +63,6 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.media.session.MediaSessionLegacyHelper;
 import android.os.Binder;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Debug;
 import android.os.FactoryTest;
@@ -100,8 +99,6 @@ import cyanogenmod.hardware.CMHardwareManager;
 import cyanogenmod.providers.CMSettings;
 import dalvik.system.DexClassLoader;
 import com.android.internal.util.temasek.DimensionConverter;
-import android.text.Html;
-import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 import android.util.Log;
@@ -7615,15 +7612,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     if (mPackageManager.isUpgrade()) {
                         mBootMsgDialog.setTitle(R.string.android_upgrading_title);
                     } else {
-                        String dialogTitle = Build.MODEL + " " + mContext.getResources().getString(
-                                com.android.internal.R.string.android_start_title);
-                        String customDialogTitle = Settings.System.getString(mContext.getContentResolver(),
-                                Settings.System.BOOT_DIALOG_TITLE);
-                        if (!TextUtils.isEmpty(customDialogTitle)) {
-                            mBootMsgDialog.setTitle(customDialogTitle);
-                        } else {
-                            mBootMsgDialog.setTitle(dialogTitle);
-                        }
+                        mBootMsgDialog.setTitle(R.string.android_start_title);
                     }
                     mBootMsgDialog.setIcon(appInfo.loadIcon(mPackageManager));
                     //mBootMsgDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
