omnirom/android_frameworks_base_old

base: SystemUI add landscape qs columns config
allow max 9 columns in landscape

Change-Id: Idde19deecfefa229307aa42bcaee9bff0a723876

core/java/android/provider/Settings.java
@@ -3878,6 +3878,12 @@ public final class Settings {
         public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
 
         /**
+         * Number of qs columns on landscape orientation
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS_LANDSCAPE = "qs_layout_columns_landscape";
+
+        /**
          * show text headers together with the stream volume images
          * default is true
          * @hide

tilaksidduram/frameworks_base

core/java/android/provider/Settings.java
@@ -6809,7 +6809,13 @@ public final class Settings {
          *
          * @hide
          */
-        public static final String QS_COLUMNS = "qs_columns";
+        public static final String QS_COLUMNS_PORTRAIT = "qs_columns_portrait";
+
+        /**
+         * Number of qs columns on landscape orientation
+         * @hide
+         */
+        public static final String QS_COLUMNS_LANDSCAPE = "qs_columns_landscape";
 
         /**
          * Whether preloaded APKs have been installed for the user.